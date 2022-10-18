bdc8bd8fc0090b0e0fe7e0fd72a4e094ee4f7974
Luc Janssen <lphjanss@gmail.com>
2022-10-18 13:57:16 +0200

    Project initialisation

---
+++ src/main/java/com/rekeningrijden/routeservice/RouteserviceApplication.java
@@ -0,0 +1,13 @@
+package com.rekeningrijden.routeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouteserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rekeningrijden/routeservice/RouteserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rekeningrijden.routeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
